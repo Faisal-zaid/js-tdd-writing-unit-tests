0324f9cd6360e0d38f07a2927cda6ddba379f081
Faisal Zaid <zaidalideveloper@gmail.com>
2025-10-20 22:15:59 +0300

    final commit

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,15 @@
 // Your code here
+export function pointsForWord(word) {
+  let points = 0;
+
+  for (const char of word) {
+    // Case-insensitive vowel check using /i
+    if (/[aeiou]/i.test(char)) {
+      points += 1; // 1 point per vowel
+    } else if (/[a-z]/i.test(char)) {
+      points += 2; // 2 points per consonant
+    }
+  }
+
+  return points;
+}
